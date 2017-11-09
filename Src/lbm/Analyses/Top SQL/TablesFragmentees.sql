f8fe317a663ffb3619ab02148d562b268f4a8bf2
yakobibus <31994217+yakobibus@users.noreply.github.com>
2017-11-09 09:22:48 +0100

    Rapport avant relecture
    Draft 0.23

--- Src/lbm/Analyses/Top SQL/TablesFragmentees.sql
+++ Src/lbm/Analyses/Top SQL/TablesFragmentees.sql
@@ -10,3 +10,14 @@ select dt.table_name
  where (round((dt.blocks * 8), 2) > round((dt.num_rows * dt.avg_row_len / 1024), 2))
  order by 4 desc
 ;
+
+select dt.owner
+     , dt.table_name
+     , round((dt.blocks * 8), 2) || 'kb' "Fragmented size"
+     , round((dt.num_rows * dt.avg_row_len / 1024), 2) || 'kb' "Actual size"
+     , round((dt.blocks * 8), 2) - round((dt.num_rows * dt.avg_row_len / 1024), 2)||'kb'
+     , ((round((dt.blocks * 8), 2) - round((dt.num_rows * dt.avg_row_len / 1024), 2)) / round((dt.blocks * 8), 2)) * 100 - 10 "reclaimable space % "
+  from dba_tables dt
+ where dt.table_name = '&table_Name'
+   AND dt.OWNER LIKE '&schema_name'
+;

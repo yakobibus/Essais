681b2b0fc4d5dc853ea013149bc12fac38df8c8d
yakobibus <31994217+yakobibus@users.noreply.github.com>
2018-03-10 22:34:55 +0100

    Valide

--- Src/ksh/prnJson.sh
+++ Src/ksh/prnJson.sh
@@ -83,25 +83,25 @@ function main {
             printf ("\t\t\t[\n")
             if (length(fieldsArray[fieldDossier]) > 0) {
                 printf ("\t\t\t\t{\n") # DOS link begin bloc
-                printf ("\t\t\t\t\t\"\" : \"%s\"\n", "TRA_VRS_DOS")
-                printf ("\t\t\t\t,\t\"\" : \"%s\"\n", "DOS")
-                printf ("\t\t\t\t,\t\"\" : \"%s\"\n", fieldsArray[fieldDossier])
+                printf ("\t\t\t\t\t\"a\" : \"%s\"\n", "TRA_VRS_DOS")
+                printf ("\t\t\t\t,\t\"b\" : \"%s\"\n", "DOS")
+                printf ("\t\t\t\t,\t\"c\" : \"%s\"\n", fieldsArray[fieldDossier])
                 printf ("\t\t\t\t}\n") # DOS link end bloc
             }
 
             if (length(fieldsArray[fieldTiers]) > 0) {
                 printf ("\t\t\t,\t{\n") # TRS link begin bloc
-                printf ("\t\t\t\t\t\"\" : \"%s\"\n", "TRA_VRS_TRS")
-                printf ("\t\t\t\t,\t\"\" : \"%s\"\n", "TRS")
-                printf ("\t\t\t\t,\t\"\" : \"%s\"\n", fieldsArray[fieldTiers])
+                printf ("\t\t\t\t\t\"a\" : \"%s\"\n", "TRA_VRS_TRS")
+                printf ("\t\t\t\t,\t\"b\" : \"%s\"\n", "TRS")
+                printf ("\t\t\t\t,\t\"c\" : \"%s\"\n", fieldsArray[fieldTiers])
                 printf ("\t\t\t\t}\n") # TRS link end bloc
             }
 
             if (length(fieldsArray[fieldContrat]) > 0) {
                 printf ("\t\t\t,\t{\n") # CNT link begin bloc
-                printf ("\t\t\t\t\t\"\" : \"%s\"\n", "TRA_VRS_CNT")
-                printf ("\t\t\t\t,\t\"\" : \"%s\"\n", "CNT")
-                printf ("\t\t\t\t,\t\"\" : \"%s\"\n", substr(fieldsArray[fieldContrat], 7, 11))
+                printf ("\t\t\t\t\t\"a\" : \"%s\"\n", "TRA_VRS_CNT")
+                printf ("\t\t\t\t,\t\"b\" : \"%s\"\n", "CNT")
+                printf ("\t\t\t\t,\t\"c\" : \"%s\"\n", substr(fieldsArray[fieldContrat], 7, 11))
                 printf ("\t\t\t\t}\n") # CNT link end bloc
             }
             printf ("\t\t\t]\n")
